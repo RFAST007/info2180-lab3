dde4dd1edffe3f61ce7c8563f815d6b71e40ac61
Rhonaldo Foster <rhonaldof@gmail.com>
2025-10-16 01:14:34 -0500

    X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,3 +8,32 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log(square);
     })
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';
+    let trackGame = []; // Track X/O in each square
+
+    squares.forEach(function(square, index) {
+        square.addEventListener('click', function() {
+            if (square.textContent=="") { // Only allow click if square is empty
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                trackGame[index] = currentPlayer;
+
+                // Alternate player
+                //currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+                if (currentPlayer === 'X') {
+                    currentPlayer = 'O';
+                } else {
+                    currentPlayer = 'X';
+                }
+            }
+
+        
+    console.log(square)
+        });
+    });
+});
+
+
